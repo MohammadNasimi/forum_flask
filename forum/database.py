db45001985e876e5cbe6de346d05e8b9cdb346c8
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-07-02 20:47:35 +0330

    add created_at

--- forum/database.py
+++ forum/database.py
@@ -5,3 +5,4 @@ class BaseModel(db.Model):
     __abstract__ = True
     
     id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.Datetime, defualt = db.fuc.current_timestamp())
