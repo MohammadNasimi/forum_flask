892b8672cd984a12c67ede5ab8c8f775e8c4ba46
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-07-03 20:42:31 +0330

    add kavenegar

--- forum/extensions.py
+++ forum/extensions.py
@@ -1,6 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from kavenegar import KavenegarAPI
+import os 
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+api = KavenegarAPI(os.getenv("API_key_kave_negar"))
 
 db = SQLAlchemy()
 migrate = Migrate()
