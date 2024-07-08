8d99fe545d92b347676bfa34e14028f53e28b169
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-07-08 18:09:16 +0330

    code model add

--- forum/users/models.py
+++ forum/users/models.py
@@ -3,8 +3,18 @@ from forum.extensions import db
 
 class User(BaseModel):
     phone = db.Column(db.String(11), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=True)
+    username = db.Column(db.String(255), unique=True, nullable=True)
     is_active = db.Column(db.Boolean(), default=True)
 
     def __repr__(self):
         return f'phone {self.phone}'
+    
+    
+class code(BaseModel):
+    code = db.Column(db.Integer)
+    expire_time = db.Column(db.DateTime,nullable=False)
+    phone = db.Column(db.String(11), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'phone {self.phone}, code {self.code}'
