99553db6a810c3e63c2ba5cbb33e1e2883e8695d
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-07-12 19:22:34 +0330

    remove validate

--- forum/users/serializer.py
+++ forum/users/serializer.py
@@ -1,7 +1,8 @@
 from forum.extensions import db
 from forum.extensions import serializer_marshmall
-
+from marshmallow import ValidationError
 from flask_marshmallow import fields
+
 class UserLogInSchema(serializer_marshmall.Schema):
     class Meta:
         fields = ('phone',)
