d4993727f34ac9862fc5be9a07be50cc310f3e1d
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-07-12 18:45:10 +0330

    use methodView

--- forum/users/routes.py
+++ forum/users/routes.py
@@ -1,21 +1,24 @@
 from flask import Blueprint
-from flask.views import View
+from flask.views import MethodView
 from .serializer import UserLogInSchema
 from flask import request ,jsonify
+from forum.extensions import serializer_marshmall
 
 users_blueprint = Blueprint('users', __name__)
 
 
-class LogInView(View):
-    methods = ["GET", "POST"]
+class LogInView(MethodView):
+    
+    def get(self):
+        return jsonify({"hi":"how adk"})
 
-    def dispatch_request(self):
-        if request.method == "GET":
-            return jsonify({"hi":"how adk"})
-        elif request.method == "POST":
-            print(request.form)
-            return jsonify({"hi":"how adk"})
-@users_blueprint.route('/login')
+    def post(self):
+        # instead request.form --> request.get_json()
+        data = request.get_json()
+        print(data.get("phone"," "))
+        return f'phone: {data}'
+    
+@users_blueprint.route('/login',methods=['GET', 'POST'])
 def log_in():
     # This function is a wrapper for the view class instance
     return LogInView.as_view('log_in')()
