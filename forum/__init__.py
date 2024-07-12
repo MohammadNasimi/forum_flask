dd2516dfd25e40d6886e97c9c2e2f37a4a4fa275
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-07-12 18:11:11 +0330

    add serializer_marshmall

--- forum/__init__.py
+++ forum/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from .users.routes import users_blueprint
 from .posts.routes import posts_blueprint
+from .users.routes import users_blueprint
 from forum.exceptions import resource_not_found,resource_sever_error
-from forum.extensions import db,migrate,login_manager
+from forum.extensions import db,migrate,serializer_marshmall
 from forum.users.models import User,code
+
 def register_error_handlers(app):
     app.register_error_handler(404,resource_not_found)
     app.register_error_handler(500,resource_sever_error)
@@ -23,14 +24,15 @@ app = Flask(__name__)
 app.register_blueprint(users_blueprint)
 app.register_blueprint(posts_blueprint)
 register_error_handlers(app)
+register_shell_context(app)
 
 app.config.from_object('config.developconfig')
 
 db.init_app(app)
+# import models 
 from .users.models import User # circular import
 migrate.init_app(app, db)
-register_shell_context(app)
-login_manager.init_app(app)
+serializer_marshmall.init_app(app)
 # with app.app_context():
 #     db.create_all() # set update model in database when run server
     
@@ -49,4 +51,7 @@ def after_request(response):
     1. flask --app forum.py db init -> create a migration repository, just one time run 
     2. flask --app forum.py db migrate -> create file for db models
     3. flask --app forum.py db upgrade -> add file model to db
+    
+    # run flask 
+    flask --app run.py run
 """
