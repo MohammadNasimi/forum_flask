2db54ebcfd45653561d3c9de1cf91ea09818af29
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-07-22 21:41:51 +0330

    CodeSchema

--- forum/users/serializer.py
+++ forum/users/serializer.py
@@ -2,8 +2,13 @@ from forum.extensions import db
 from forum.extensions import serializer_marshmall
 from marshmallow import ValidationError
 from flask_marshmallow import fields
-from .models import User
+from .models import User,code
 class UserLogInSchema(serializer_marshmall.Schema):
     class Meta:
         model = User
         fields = ('phone',)
+        
+class CodeSchema(serializer_marshmall.Schema):
+    class Meta:
+        model = code
+        fields = ('number',)
