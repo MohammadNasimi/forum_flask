65af17c77ff5c680f8187874751402b36b12c9b4
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-07-22 21:41:42 +0330

    LogInView refactor check code exist or not

--- forum/users/routes.py
+++ forum/users/routes.py
@@ -1,13 +1,13 @@
 from flask import Blueprint,session
-from flask.views import MethodView
-from .serializer import UserLogInSchema
+from flask.views import MethodView,View
+from .serializer import UserLogInSchema,CodeSchema
 from flask import request ,jsonify
 from marshmallow import ValidationError
 import random 
 from datetime import datetime ,timedelta
 
 from forum.extensions import db, sms_api
-from .models import code
+from .models import code,User
 
 
 users_blueprint = Blueprint('users', __name__)
@@ -36,13 +36,32 @@ class LogInView(MethodView):
         # params = {'sender':'', 'receptor':int(phone), 'message':rand_num}
         # sms_api.sms_send(params)
         print(rand_num)
-        code_phone = code(
-                    number=rand_num,
-                    expire_time=datetime.now() + timedelta(minutes=10),
-                    phone = phone
-                    )
-        db.session.add(code_phone)
-        db.session.commit()
+        # check exist code in db for create or delete
+        print(bool(code.query.filter_by(phone=phone).first()))
+        if bool(code.query.filter_by(phone=phone).first()):
+            code_phone = code.query.filter_by(phone=phone).first()
+            print(code_phone.expire_time < datetime.now())
+            print(code_phone.expire_time)
+            if not code_phone.expire_time < datetime.now():
+                return jsonify({"message":"can't get code phone try again afew min ago"})
+            else:
+                db.session.delete(code_phone)
+                db.session.commit()
+                code_phone = code(
+                            number=rand_num,
+                            expire_time=datetime.now() + timedelta(minutes=10),
+                            phone = phone
+                            )
+                db.session.add(code_phone)
+                db.session.commit()
+        else:
+            code_phone = code(
+                            number=rand_num,
+                            expire_time=datetime.now() + timedelta(minutes=10),
+                            phone = phone
+                            )
+            db.session.add(code_phone)
+            db.session.commit()
         return jsonify({"message":f"send code to phone -> {phone}"})
 
         
@@ -51,3 +70,36 @@ class LogInView(MethodView):
 def log_in():
     # This function is a wrapper for the view class instance
     return LogInView.as_view('log_in')()
+
+
+
+class CodePhoneView(View):
+    def dispatch_request(self):
+        code_ = request.get_json()
+        schema = CodeSchema()
+        try:
+            valid_code = schema.load(code_)
+        except ValidationError as err:
+            # Handle validation errors
+            return f"Error: {err}", 400
+        # check correct code 
+        code_phone = code.query.filter_by(phone=session.get('user_phone')).first()
+        if code_phone.expire_time < datetime.now():
+            return jsonify({"message":"Expiration Error, please try again"})
+        if valid_code["number"] != str(code_phone.number):
+            return jsonify({"message":"your code is wronge"})
+        else:
+            user = User.query.filter_by(phone=session.get('user_phone')).first()
+            if not user:
+                # Create user if not found
+                new_user = User(phone=session.get('user_phone'))
+                db.session.add(new_user)
+                db.session.commit()
+                return jsonify({"message":new_user.phone})
+            else:
+                return jsonify({"message":user.phone})
+        
+@users_blueprint.route('/code',methods=['POST'])
+def get_code():
+    # This function is a wrapper for the view class instance
+    return CodePhoneView.as_view('CodePhoneView')()
