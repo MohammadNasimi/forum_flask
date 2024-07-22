09997e3e57f1d58ba556df7c2d949be781316231
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-07-22 20:23:05 +0330

    complete log in view

--- forum/users/routes.py
+++ forum/users/routes.py
@@ -1,8 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint,session
 from flask.views import MethodView
 from .serializer import UserLogInSchema
 from flask import request ,jsonify
 from marshmallow import ValidationError
+import random 
+from datetime import datetime ,timedelta
+
+from forum.extensions import db, sms_api
+from .models import code
 
 
 users_blueprint = Blueprint('users', __name__)
@@ -11,7 +16,8 @@ users_blueprint = Blueprint('users', __name__)
 class LogInView(MethodView):
     
     def get(self):
-        return jsonify({"hi":"how adk"})
+        user_phone = session.get('user_phone',None)
+        return jsonify({"log in this phone":f"{user_phone}"})
 
     def post(self):
         # check correct data
@@ -19,14 +25,28 @@ class LogInView(MethodView):
         data = request.get_json()
         schema = UserLogInSchema()
         try:
-            schema.load(data)
+            valid_data = schema.load(data)
         except ValidationError as err:
             # Handle validation errors
             return f"Error: {err}", 400
+        # create code 
+        rand_num = random.randint(1000, 9999)
+        phone = valid_data.get("phone"," ")
+        session['user_phone'] = phone
+        # params = {'sender':'', 'receptor':int(phone), 'message':rand_num}
+        # sms_api.sms_send(params)
+        print(rand_num)
+        code_phone = code(
+                    number=rand_num,
+                    expire_time=datetime.now() + timedelta(minutes=10),
+                    phone = phone
+                    )
+        db.session.add(code_phone)
+        db.session.commit()
+        return jsonify({"message":f"send code to phone -> {phone}"})
+
         
-        # print(data.get("phone"," "))
-        return f'phone: {data}',200
-    
+        
 @users_blueprint.route('/login',methods=['GET', 'POST'])
 def log_in():
     # This function is a wrapper for the view class instance
