73719716594b60df6941bcf59430a6de92c6372f
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-07-26 11:50:31 +0330

    add model Follow

--- forum/users/models.py
+++ forum/users/models.py
@@ -21,3 +21,14 @@ class code(BaseModel):
 
     def __repr__(self):
         return f'phone {self.phone}, code {self.number}'
+    
+
+
+
+class Follow(BaseModel):
+	follower = db.Column(db.Integer, db.ForeignKey('user.id'))
+	followed = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+	def __repr__(self):
+		return f'{self.__class__.__name__} ({self.follower}, {self.followed})'
